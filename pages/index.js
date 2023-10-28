eba888507254eda6d9ec6393d2b654a0d999d277
alperhasturk <112662258+alperhasturk@users.noreply.github.com>
2023-10-29 01:41:42 +0300

    fixing issues

--- pages/index.js
+++ pages/index.js
@@ -102,17 +102,17 @@ export default function Home() {
             <h4 className="text-lg pt-6 text-gray-500">Scroll down to check out my projects</h4>
             <svg
               height={70}
-              clip-rule="evenodd"
-              fill-rule="evenodd"
-              stroke-linejoin="round"
-              stroke-miterlimit="2"
+              clipRule="evenodd"
+              fillRule="evenodd"
+              strokeLinejoin="round"
+              strokeMiterlimit="2"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
               className="animate-bounce mt-24 border-2 fill-current text-orange-500 border-orange-500 rounded-full p-2 hover:bg-orange-500 hover:text-bg hover:border-bg"
             >
               <path
                 d="m9.001 13.022h-3.251c-.412 0-.75.335-.75.752 0 .188.071.375.206.518 1.685 1.775 4.692 4.945 6.069 6.396.189.2.452.312.725.312.274 0 .536-.112.725-.312 1.377-1.451 4.385-4.621 6.068-6.396.136-.143.207-.33.207-.518 0-.417-.337-.752-.75-.752h-3.251v-9.02c0-.531-.47-1.002-1-1.002h-3.998c-.53 0-1 .471-1 1.002z"
-                fill-rule="nonzero"
+                fillRule="nonzero"
               />
             </svg>
           </div>
